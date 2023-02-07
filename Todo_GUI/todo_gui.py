c147cefba50f40eb066d17531951df8529932e6d
belalraouf <belal.raouf1@gmial.com>
2023-02-07 16:58:10 -0500

    Testing with Print Statement how the app works.

--- Todo_GUI/todo_gui.py
+++ Todo_GUI/todo_gui.py
@@ -4,6 +4,7 @@ import PySimpleGUI as sg
 label = sg.Text("Type To-Do's Here: ")
 input_box = sg.InputText(tooltip="Enter todos ")
 add_button = sg.Button("Add")
-window = sg.Window("My To-Do App", layout = [[label],[input_box, add_button]])
+window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
 window.read()
+print("This program works good!")
 window.close()
