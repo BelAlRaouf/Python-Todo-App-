f7025269192189e95253f14143ed3db59971ac7d
belalraouf <belal.raouf1@gmial.com>
2023-02-08 18:31:06 -0500

    The of the program changed to Dark Grey 13.

--- Todo_GUI/todo_gui.py
+++ Todo_GUI/todo_gui.py
@@ -1,6 +1,7 @@
 import functions
 import PySimpleGUI as sg
 
+sg.theme("dark grey 13")
 label = sg.Text("Type To-Do's Here: ")
 input_box = sg.InputText(tooltip="Enter todos ", key="todo")
 add_button = sg.Button("Add")
@@ -11,7 +12,6 @@ edit_button = sg.Button("Edit")
 
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
-
 window = sg.Window("My To-Do App",
                    layout=[[label],
                            [input_box, add_button],
@@ -20,7 +20,6 @@ window = sg.Window("My To-Do App",
                    font=("Arial", 20))
 
 while True:
-
     event, values = window.read()
     print(1, event)
     print(2, values)
@@ -34,26 +33,31 @@ while True:
             window['todos'].update(values=todos)
 
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
-            todos = functions.read_file()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_file(todos)
-            window['todos'].update(values=todos)
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+                todos = functions.read_file()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_file(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select the item first.", font=("Arial", 15))
 
         case "todos":
             window['todo'].update(value=values['todos'][0])
 
         case "Complete":
-            todo_to_complete = values['todos'][0]
-
-            todos = functions.read_file()
-            index = todos.index(todo_to_complete)
-            removed_item = todos.pop(index)
-            functions.write_file(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value="")
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.read_file()
+                index = todos.index(todo_to_complete)
+                removed_item = todos.pop(index)
+                functions.write_file(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
+            except IndexError:
+                sg.popup("Please select the item first.", font=("Arial", 15))
 
         case "Exit":
             break
